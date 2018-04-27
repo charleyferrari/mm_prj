06f4234b11d7da9d845f1c8b44a49d1f38a7271f
Charley Ferrari <charley@charleyferrari.com>
2018-04-27 14:14:26 -0400

    iteritems -> items

--- start.py
+++ start.py
@@ -90,11 +90,11 @@ def econ_api():
     data_cum_dict = data_pivot.to_dict(orient='records')
     # Get rid of numpy types
     for item in data_cum_dict:
-        for key, val in item.iteritems():
+        for key, val in item.items():
             val = str(val) if key == 'economic_stability' else int(val)
     data_vals_dict = data_pivot_vals.to_dict(orient='records')
     for item in data_vals_dict:
-        for key, val in item.iteritems():
+        for key, val in item.items():
             val = str(val) if key == 'economic_stability' else int(val)
 
     data_dict = [dict(val=val, cum=cum)
@@ -155,8 +155,6 @@ def box_api():
 
     min_y = min([i['min'] for i in data])
     max_y = max([i['max'] for i in data])
-
-    print(type(data[0]['p75']))
 
     return jsonify(dict(data=data, min_y=min_y, max_y=max_y))
 
